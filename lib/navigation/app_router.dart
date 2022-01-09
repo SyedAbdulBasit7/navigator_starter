cfc82bb79f9a692bfa1946d5a9331df4169c0d10
SyedAbdulBasit7 <syedspy1444@gmail.com>
2022-01-09 23:54:07 +0500

    Initial commit

---
+++ lib/navigation/app_router.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/models/models.dart';
+
+class AppRouter extends RouterDelegate
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin {
+  @override
+  final GlobalKey<NavigatorState> navigatorKey;
+
+  final AppStateManager appStateManager;
+  final GroceryManager groceryManager;
+  final ProfileManager profileManager;
+
+  AppRouter({
+    required this.appStateManager,
+    required this.groceryManager,
+    required this.profileManager,
+  }) : navigatorKey = GlobalKey<NavigatorState>() {
+    appStateManager.addListener(notifyListeners);
+    groceryManager.addListener(notifyListeners);
+    profileManager.addListener(notifyListeners);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      key: navigatorKey,
+      onPopPage: _handlePopPage,
+      pages: [],
+    );
+  }
+
+  @override
+  void dispose() {
+    appStateManager.removeListener(notifyListeners);
+    groceryManager.removeListener(notifyListeners);
+    profileManager.removeListener(notifyListeners);
+  }
+
+  bool _handlePopPage(Route<dynamic> route, result) {
+    if (!route.didPop(result)) {
+      return false;
+    }
+    return true;
+  }
+
+  @override
+  Future<void> setNewRoutePath(configuration) async {
+    return null;
+  }
+}
